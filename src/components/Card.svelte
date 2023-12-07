c6581544a61a707bbbeff14a11319209f6b5d3f2
Mike Peiman <mikepeiman@gmail.com>
2023-12-07 15:19:29 -0500

    added a few notes

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,17 +1,22 @@
 <script>
     let title = "Svelte"
     let description = "The official Svelte app framework powered by Vite!"
+// clamp the description to fit in the card 
+    description = description.substring(0, 100) + "..."
+
 </script>
 
 <div class="flex card-wrapper">
     <div class="image-wrapper flex">
-        <img src="https://loremflickr.com/320/240?madeitup={Math.random()}" alt="Placeholder image from Flickr - proably a random cat">
+        <img src="https://loremflickr.com/320/240?t={Math.random()}" alt="Placeholder image from Flickr - proably a random cat">
     </div>
     <div class="card-content-wrapper">
         <h1>{title}</h1>
         <p>{description}</p>
     </div>
 </div>
+
+
 
 <style>
 
@@ -20,6 +25,7 @@
         height: auto;
         background: #ccc;
         border-radius: 0 1rem 1rem 0;
+        height: 10rem;
     }
     :global(.flex) {
         display: flex;
@@ -28,7 +34,9 @@
     .image-wrapper {
         width: 30%;
         height: 100%;
-
+        background: #333;
+        width: 10rem;
+        height: 10rem;
     }
 
     img {
@@ -48,9 +56,13 @@
 
     h1 {
         font-size: 2rem;
+        margin: 0;
     }
 
     p {
-        font-size: 1.2rem;
+        /* font-size: 1.2rem; */
+        margin: 0;
+        padding: 0.25rem;
+        padding-top: .5rem;
     }
 </style>
