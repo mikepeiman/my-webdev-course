6b919e701eee362b061ac246430385608b08c2ae
Mike Peiman <mikepeiman@gmail.com>
2023-12-10 16:05:40 -0500

    doing a bit of Canvas, but failing to get simple intended result of a circle

---
+++ src/routes/exercises/2023-12-10/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import QuickCanvas from './QuickCanvas.svelte';
+
+    let content = {
+        date: "2023-12-10",
+        tags: ["skeleton-ui", 'tailwindcss','sveltekit'],
+        title: "Getting started with skeleton-ui, tailwindcss, sveltekit",
+        main: "This is the main content"
+    }
+
+    
+
+</script>
+
+<div class="container flex justify-center flex-col items-center">
+    <h1 class="date-item">Today - {content.date}</h1>
+    <p class="title">{content.title}</p>
+    <p class="content">{content.main}</p>
+</div>
+
+<QuickCanvas {content} />
+
+<style>
+    .date-item {
+        @apply text-xl;
+    }
+    .title {
+        @apply text-3xl;
+        
+    }
+    .content {
+        @apply text-lg;
+    }
+    
+</style>
---
+++ src/routes/exercises/2023-12-10/QuickCanvas.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log(`canvas component mounted`);
+        sketchOnCanvas();
+	});
+
+    // create reactive variables for screen width and height, but that checks if window is defined
+    let width = 0;
+    let height = 0;
+    $: if (typeof window !== 'undefined') {
+        width = window.innerWidth;
+        console.log(`🚀 ~ file: QuickCanvas.svelte:14 ~ width:`, width)
+        height = window.innerHeight;
+        console.log(`🚀 ~ file: QuickCanvas.svelte:16 ~ height:`, height)
+    }
+    
+	function sketchOnCanvas() {
+        let c = document.getElementById('canvas-1');
+        console.log(`🚀 ~ file: QuickCanvas.svelte:21 ~ sketchOnCanvas ~ c:`, c)
+        let ctx = c.getContext('2d');
+        let center = {
+            x: width / 2,
+            y: height / 2
+        }
+        console.log(`🚀 ~ file: QuickCanvas.svelte:26 ~ sketchOnCanvas ~ center:`, center)
+		ctx.beginPath();
+        console.log(`🚀 ~ file: QuickCanvas.svelte:29 ~ sketchOnCanvas ~ ctx:`, ctx)
+        
+		ctx.arc(center.x, center.y, center.y / 2, 0, 2 * Math.PI);
+        ctx.strokeStyle = '#af00f0';
+        ctx.fillStyle = 'green';
+        ctx.lineWidth = 10;
+		ctx.stroke();
+        ctx.fill();
+	}
+</script>
+
+<canvas id="canvas-1" />
+
+<style>
+	#canvas-1 {
+		width: 100%;
+		height: 100%;
+		display: block;
+		box-shadow: 0px 2px 12px -2px rgba(0, 0, 0, 0.15);
+		margin: 0;
+		padding: 0;
+		border: 0;
+		outline: 0;
+		background: #111;
+	}
+</style>
