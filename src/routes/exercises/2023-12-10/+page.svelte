1875932ebf11ec59ab1369bb061ee1abb1e3ad73
Mike Peiman <mikepeiman@gmail.com>
2023-12-10 16:32:54 -0500

    finally got the circle rendering!

--- src/routes/exercises/2023-12-10/+page.svelte
+++ src/routes/exercises/2023-12-10/+page.svelte
@@ -2,11 +2,20 @@
     import { Canvas, Layer } from 'svelte-canvas';
   
     const render = ({ context, width, height }) => {
-      context.font = `${width / 10}px sans-serif`;
-      context.textAlign = 'center';
-      context.textBaseline = 'middle';
-      context.fillStyle = 'tomato';
-      context.fillText('Canvas, finally', width / 2, height / 2);
+		context.clearRect(0, 0, width, height);
+		context.beginPath();
+		console.log(`🚀 ~ file: QuickCanvas.svelte:29 ~ sketchOnCanvas ~ context:`, context);
+
+		context.arc(width / 2, height / 2, height / 4, 0, 2 * Math.PI);
+		context.strokeStyle = '#af00f0';
+		context.fillStyle = '#00acff';
+		context.lineWidth = 10;
+		context.stroke();
+		context.fill();
+
+		context.lineTo(width, height);
+		context.lineTo(0, height);
+		context.lineTo(width, 0);
     };
   </script>
   
