c5975c2834ab938f4f8a554d95ce72cd624ca746
Mike Peiman <mikepeiman@gmail.com>
2023-12-10 17:26:40 -0500

    got some animated arc-orbit working

--- src/routes/exercises/2023-12-10/+page.svelte
+++ src/routes/exercises/2023-12-10/+page.svelte
@@ -1,25 +1,45 @@
 <script>
-    import { Canvas, Layer } from 'svelte-canvas';
-  
-    const render = ({ context, width, height }) => {
-		context.clearRect(0, 0, width, height);
-		context.beginPath();
-		console.log(`🚀 ~ file: QuickCanvas.svelte:29 ~ sketchOnCanvas ~ context:`, context);
+	import { Canvas, Layer } from 'svelte-canvas';
+	// import Orbits1 from './Orbits1.svelte';
 
-		context.arc(width / 2, height / 2, height / 4, 0, 2 * Math.PI);
+    let speck = {
+        arc: {
+            start: 0,
+            end: 2 * Math.PI / 60
+        },
+        speed: .005,
+        angle: 0,
+
+    }
+
+    const update = (time) => {
+        // speck.angle += speck.speed;
+        let d = speck.arc.end - speck.arc.start;
+
+        speck.arc.start += time;
+        speck.arc.end +=  time;
+    }
+
+	const render = ({ context, width, height, time }) => {
+		// console.log(`🚀 ~ file: +page.svelte:7 ~ render ~ time:`, time)
+		// context.clearRect(0, 0, width, height);
+        let moment = time % (60) / 3600;
+        update(moment);
+		context.save();
+		context.beginPath();
+
+		context.arc(width / 2, height / 2, height / 4, speck.arc.start, speck.arc.end);
 		context.strokeStyle = '#af00f0';
 		context.fillStyle = '#00acff';
-		context.lineWidth = 10;
+		context.lineWidth = 250;
 		context.stroke();
-		context.fill();
+		context.restore();
+	};
+</script>
 
-		context.lineTo(width, height);
-		context.lineTo(0, height);
-		context.lineTo(width, 0);
-    };
-  </script>
-  
-  <Canvas autoplay>
-    <Layer {render} />
-  </Canvas>
-  
+<!-- <Orbits1 /> -->
+<!-- <Orbits2 {render} /> -->
+
+<Canvas autoplay>
+	<Layer {render} />
+</Canvas>
