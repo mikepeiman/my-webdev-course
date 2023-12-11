b47ca93065974e100285ac530a7dccd4a0b31ce7
Mike Peiman <mikepeiman@gmail.com>
2023-12-11 14:19:45 -0500

    some success animating, but getting the fuzzy effect. going to explore some libraries

--- src/routes/exercises/2023-12-11/3/+page.svelte
+++ src/routes/exercises/2023-12-11/3/+page.svelte
@@ -11,7 +11,9 @@
         const centerX = canvas.width / 2;
         const centerY = canvas.height / 2;
 
-        function animate(timestamp) {
+        let planetPath = new Path2D('M17.9 17.39c-.26-.8-1.01-1.39-1.9-1.39h-1v-3a1 1 0 0 0-1-1H8v-2h2a1 1 0 0 0 1-1V7h2a2 2 0 0 0 2-2v-.41a7.984 7.984 0 0 1 2.9 12.8M11 19.93c-3.95-.49-7-3.85-7-7.93c0-.62.08-1.22.21-1.79L9 15v1a2 2 0 0 0 2 2m1-16A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2');
+
+        function animate() {
             context.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
 
             // calculate new position
@@ -20,6 +22,9 @@
 
             // draw dot
             context.beginPath();
+            context.strokeStyle = `hsl(${time * 100}, 100%, 50%)`;
+            context.stroke(planetPath);
+
             context.arc(x, y, 2, 0, 2 * Math.PI);
             context.fillStyle = `hsl(${time * 100}, 100%, 50%)`;
             context.fill();
