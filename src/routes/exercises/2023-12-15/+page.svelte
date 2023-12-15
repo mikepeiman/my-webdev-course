421f7ef06b027a462eb0dd677cf96ece59a51ce5
Mike Peiman <mikepeiman@gmail.com>
2023-12-15 17:31:34 -0500

    trying a fresh start at drawing an svg-based hex grid

--- src/routes/exercises/2023-12-15/+page.svelte
+++ src/routes/exercises/2023-12-15/+page.svelte
@@ -1,153 +1,133 @@
 <script>
 	import { onMount } from 'svelte';
-	import anime from 'animejs';
-	import { Pane } from 'tweakpane';
-	// your script goes here
 
-	let data = {
-		numHexes: 1
+	$: radius = 100;
+	$: centerX = 150;
+	$: centerY = 150;
+	$: cols = PARAMS.numHexesInCol;
+	$: rows = PARAMS.numHexesInRow;
+	$: hexagonSize = PARAMS.hexagonSize;
+	$: hexagonSpacing = PARAMS.hexagonSpacing;
+	$: hexagonColor = PARAMS.hexagonColor;
+	$: hexagonStrokeColor = PARAMS.hexagonStrokeColor;
+	$: hexagonStrokeWidth = PARAMS.hexagonStrokeWidth;
+	$: hexagonStrokeOpacity = PARAMS.hexagonStrokeOpacity;
+	$: hexagonOpacity = PARAMS.hexagonOpacity;
+
+	let PARAMS = {
+		radius: 100,
+		centerX: 150,
+		centerY: 150,
+		numHexesInRow: 5,
+		numHexesInCol: 5,
+		hexagonSize: 50,
+		hexagonSpacing: 10,
+		hexagonColor: 'black',
+		hexagonStrokeColor: 'white',
+		hexagonStrokeWidth: 2,
+		hexagonStrokeOpacity: 1,
+		hexagonOpacity: 1
 	};
-	$: settings = {
-		targets: ['.svg-attributes-demo polygon', 'feTurbulence', 'feDisplacementMap'],
-		points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
-		baseFrequency: 0,
-		scale: 1,
-		loop: PARAMS.loop,
-		direction: 'alternate',
-		easing: 'easeInOutExpo'
-	};
 
-
-
-	$: hue1 = Math.floor(Math.random() * 360);
-
-	
-	$: PARAMS = {
-		percentage: 100,
-		hitpoints: 50,
-		diamter: 50,
-		loop: false,
-		theme: 'dark'
-	};
-	
-	// `min` and `max`: slider
-	onMount(() => {
-		const pane = new Pane({
-			title: 'Parameters',
-			expanded: true
-		});
-		function fitElementToParent(el, padding) {
-			var timeout = null;
-			function resize() {
-				if (timeout) clearTimeout(timeout);
-				anime.set(el, { scale: 1 });
-				var pad = padding || 0;
-				var parentEl = el.parentNode;
-				var elOffsetWidth = el.offsetWidth - pad;
-				var parentOffsetWidth = parentEl.offsetWidth;
-				var ratio = parentOffsetWidth / elOffsetWidth;
-				timeout = setTimeout(anime.set(el, { scale: ratio }), 10);
-			}
-			resize();
-			window.addEventListener('resize', resize);
+	// Function to calculate the points of the hexagon
+	function calculateHexagonPoints(radius, centerX, centerY) {
+		let points = [];
+		for (let i = 0; i < 6; i++) {
+			const angle = Math.PI / 6 + (Math.PI / 3) * i;
+			const x = centerX + radius * Math.cos(angle);
+			const y = centerY + radius * Math.sin(angle);
+			points.push(`${x},${y}`);
 		}
-		
-		pane.addBinding(PARAMS, 'percentage', { min: 0, max: 100, step: 10 });
-		pane.addBinding(PARAMS, 'hitpoints', { min: 0, max: 100, step: 10 });
-		pane.addBinding(PARAMS, 'diamter', { min: 0, max: 100, step: 10 });
-		pane.addBinding(PARAMS, 'loop');
-		anime(settings);
-		
+		return points.join(' ');
+	}
+	onMount(() => {
+		// Create the SVG element
+		// const svgNS = 'http://www.w3.org/2000/svg';
+		// let svg = document.createElementNS(svgNS, 'svg');
+		// svg.setAttribute('width', '300');
+		// svg.setAttribute('height', '300');
+		// svg.setAttribute('viewBox', '0 0 300 300');
+
+		// // Create the polygon (hexagon) element
+		// let polygon = document.createElementNS(svgNS, 'polygon');
+		// polygon.setAttribute(
+		// 	'points',
+		// 	calculateHexagonPoints(PARAMS.radius, PARAMS.centerX, PARAMS.centerY)
+		// );
+		// polygon.setAttribute('stroke', PARAMS.hexagonStrokeColor);
+		// polygon.setAttribute('fill', PARAMS.hexagonColor);
+
+		// // Append the polygon to the SVG
+		// svg.appendChild(polygon);
+
+		// // Append the SVG to the body (or to a specific element in your page)
+		// let hexGrid = document.getElementById('hexGrid');
+		// hexGrid.appendChild(svg);
+
+		// Create the hexagons using Hexagon class and PARAMS.numHexesInRow and PARAMS.numHexesInCol
+		for (let i = 0; i < PARAMS.numHexesInRow; i++) {
+			for (let j = 0; j < PARAMS.numHexesInCol; j++) {
+				// const hex = new Hexagon(PARAMS.hexagonSize, PARAMS.centerX + PARAMS.hexagonSpacing * j, PARAMS.centerY + PARAMS.hexagonSpacing * i);
+				// hex.draw();
+			}
+
+		}
 	});
+
+	const createNewHex = () => {
+		const hex = new Hexagon(PARAMS.radius, PARAMS.centerX, PARAMS.centerY);
+		console.log(`🚀 ~ file: +page.svelte:79 ~ createNewHex ~ hex:`, hex)
+		hex.draw();
+	};
+
+
+	class Hexagon {
+		constructor(radius, centerX, centerY) {
+			this.radius = radius;
+			this.centerX = centerX;
+			this.centerY = centerY;
+			this.points = calculateHexagonPoints(radius, centerX, centerY);
+		}
+
+		draw(el) {
+			const svgNS = 'http://www.w3.org/2000/svg';
+			let svg = document.createElementNS(svgNS, 'svg');
+			svg.setAttribute('width', PARAMS.numHexesInRow * PARAMS.radius * 2);
+			svg.setAttribute('height', PARAMS.numHexesInCol * PARAMS.radius * 2);
+			svg.setAttribute('viewBox', `0 0 ${PARAMS.radius * 2} ${PARAMS.radius * 2}`);
+
+			// Create the polygon (hexagon) element
+			let polygon = document.createElementNS(svgNS, 'polygon');
+			polygon.setAttribute('points', this.points);
+			polygon.setAttribute('stroke', PARAMS.hexagonStrokeColor);
+			polygon.setAttribute('fill', PARAMS.hexagonColor);
+
+			// Append the polygon to the SVG
+			svg.appendChild(polygon);
+
+			// Append the SVG to the body (or to a specific element in your page)
+			let hexGrid = document.getElementById('hexGrid');
+			el.appendChild(svg);
+
+		}
+	}
 </script>
 
-<div>
-	{#each Array(data.numHexes) as hex, i}
-		<div class="demo-content align-center svg-attributes-demo">
-			<svg width="100%" height="100%" viewBox="0 0 128 128">
-				<filter id="displacementFilter">
-					<feTurbulence
-						type="turbulence"
-						baseFrequency="0.04649878370558896"
-						numOctaves="2"
-						result="turbulence"
-						style="transform: scale(1);"
-					/>
-					<feDisplacementMap
-						in2="turbulence"
-						in="SourceGraphic"
-						scale="14.019659437564908"
-						xChannelSelector="R"
-						yChannelSelector="G"
-					/>
-				</filter>
-				<polygon
-					on:load={() => console.log(`hex ${i} loaded`)}
-					points="64 72.7966439847248 8.574 99.71990269644712 59.60362518986154 65.18153205230828 64 3.7199026964471162 68.39637481013845 65.18153205230828 119.426 99.71990269644712 "
-					style="filter: url(&quot;#displacementFilter&quot;); transform: scale({PARAMS.percentage / 100});"
-					fill="hsla({hue1}, 50%, 50%, 0.5)"
-				/>
-			</svg>
-		</div>
-	{/each}
-</div>
+<div class="input-group-wrapper">
+   <input type="range" min="0" max="100" bind:value={PARAMS.radius} />
+   <input type="range" min="0" max="100" bind:value={PARAMS.centerX} />
+   <input type="range" min="0" max="100" bind:value={PARAMS.centerY} />
+   <input type="range" min="0" max="100" bind:value={PARAMS.numHexesInRow} />
 
-<style>
-	:global(#page-content) {
-		height: calc(100vh - 5rem);
-	}
-
-	:global(.tp-dfwv) {
-		top: 5rem !important;
-	}
-
-	.hex-grid {
-		display: grid;
-		grid-template-columns: repeat(12, 1fr);
-		grid-template-rows: repeat(12, 1fr);
-	}
-
-	.demo-content {
-		position: relative;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		text-align: center;
-		width: 100%;
-		color: #00f;
-		background: linear-gradient(45deg, hsla(330, 50%, 50%, 0.5), hsla(90, 50%, 50%, 0.5)),
-			linear-gradient(45deg, hsla(0, 50%, 50%, 0.5), hsla(0, 50%, 51800%, 0.5));
-		background: #222;
-
-		height: 100%;
-	}
-
-	.demo-content svg polygon {
-		background: linear-gradient(45deg, hsla(130, 50%, 50%, 0.5), hsla(190, 50%, 50%, 0.5)),
-			linear-gradient(45deg, hsla(0, 50%, 50%, 0.5), hsla(0, 50%, 51800%, 0.5));
-		fill: #111;
-		stroke: hsla(200, 100%, 50%, 1);
-		stroke-width: 1;
-		stroke-linejoin: round;
-		stroke-linecap: round;
-	}
-
-	.svg-attributes-demo {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.svg-attributes-demo svg {
-		width: 50%;
-		height: 50%;
-	}
-
-	.svg-attributes-demo polygon {
-		fill: none;
-		stroke: #fff;
-		stroke-width: 2;
-		stroke-linejoin: round;
-		stroke-linecap: round;
-	}
-</style>
+   </div>
+<div id="hexGrid" class="flex flex-wrap">
+{#each Array(cols) as col}
+{#each Array(rows) as row}
+	 <!-- content here -->
+	 <div class="hex" id="hex-{row}-{col}" on:load={() => createNewHex(this)}>
+		
+	 </div>
+	 
+	 {/each}
+{/each}
+	 </div>
---
+++ src/routes/exercises/2023-12-15/notes.md
@@ -0,0 +1,5 @@
+Thinking about the process to develop a hex-grid-based game, as a learning path for Kaya and I in our webdev course.
+
+1. Start with a container holding a square grid pattern. Integrate tweakpane or similar controls.
+2. Offset even or odd rows or columns (user selection, variables)
+3. Draw hexes on the points
