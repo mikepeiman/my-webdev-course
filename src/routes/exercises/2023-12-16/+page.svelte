aa34f6ef742bb7da6ba1dad25bdfac85000e5e97
Mike Peiman <mikepeiman@gmail.com>
2023-12-16 11:43:56 -0500

    got first offset grid working. still have gap to resolve

--- src/routes/exercises/2023-12-16/+page.svelte
+++ src/routes/exercises/2023-12-16/+page.svelte
@@ -26,7 +26,7 @@
 		let points = [];
 		for (let i = 0; i < 6; i++) {
 			const angle = Math.PI / 6 + (Math.PI / 3) * i;
-			const x = centerX + radius * Math.cos(angle) ;
+			const x = centerX + radius * Math.cos(angle);
 			const y = centerY + radius * Math.sin(angle);
 			points.push(`${x},${y}`);
 		}
@@ -48,29 +48,38 @@
 
 	const generateHexGridCoordinates = (radius, centerX, centerY, cols, rows) => {
 		// Create the hexagons coordinates array so they can all be drawn at once
+		let newCenterX = centerX + radius;
 		hexGridCoordinates = [];
 		for (let i = 0; i < cols; i++) {
 			for (let j = 0; j < rows; j++) {
 				// if odd row, offset x
-				// if (j % 2 === 1) {
-				// 	centerX = centerX + radius;
-				// }
-				const hex = new Hexagon(
-					radius,
-					(centerX + PARAMS.hexagonSpacing)  * (j + 1),
-					(centerY + PARAMS.hexagonSpacing)  * (i + 1),
-					i,
-					j
-				);
-				hexGridCoordinates.push(hex);
+				if (i % 2 === 1) {
+					const hex = new Hexagon(
+						radius,
+						(centerX + PARAMS.hexagonSpacing) * (j + 1) + radius,
+						(centerY + PARAMS.hexagonSpacing) * (i + 1),
+						i,
+						j
+					);
+					hexGridCoordinates.push(hex);
+				} else {
+					const hex = new Hexagon(
+						radius,
+						(centerX + PARAMS.hexagonSpacing) * (j + 1),
+						(centerY + PARAMS.hexagonSpacing) * (i + 1),
+						i,
+						j
+					);
+					hexGridCoordinates.push(hex);
+				}
 			}
 		}
 		return hexGridCoordinates;
 	};
 
 	const drawAllHexes = () => {
-		let width = PARAMS.numHexesInRow * PARAMS.radius * 2 + PARAMS.hexagonSpacing + PARAMS.radius
-		let height = PARAMS.numHexesInCol * PARAMS.radius * 2  + PARAMS.hexagonSpacing + PARAMS.radius
+		let width = PARAMS.numHexesInRow * PARAMS.radius * 2 + PARAMS.hexagonSpacing + PARAMS.radius;
+		let height = PARAMS.numHexesInCol * PARAMS.radius * 2 + PARAMS.hexagonSpacing + PARAMS.radius;
 		const svgNS = 'http://www.w3.org/2000/svg';
 		let svg = document.createElementNS(svgNS, 'svg');
 		svg.setAttribute('width', width);
@@ -83,31 +92,13 @@
 
 		let hexGrid = document.getElementById('hexGrid');
 		hexGrid.appendChild(svg);
-
-		// const svgNS = 'http://www.w3.org/2000/svg';
-		// let svg = document.createElementNS(svgNS, 'svg');
-		// svg.setAttribute('width', PARAMS.numHexesInRow * PARAMS.radius * 2);
-		// svg.setAttribute('height', PARAMS.numHexesInCol * PARAMS.radius * 2);
-		// svg.setAttribute('viewBox', `0 0 ${PARAMS.radius * 2} ${PARAMS.radius * 2}`);
-
-		// // Create the polygon (hexagon) element
-		// let polygon = document.createElementNS(svgNS, 'polygon');
-		// polygon.setAttribute('points', this.points);
-		// polygon.setAttribute('stroke', PARAMS.hexagonStrokeColor);
-		// polygon.setAttribute('fill', PARAMS.hexagonColor);
-
-		// // Append the polygon to the SVG
-		// svg.appendChild(polygon);
-
-		// // Append the SVG to the body (or to a specific element in your page)
-		// let hexGrid = document.getElementById('hexGrid');
 	};
 
 	class Hexagon {
 		constructor(radius, centerX, centerY, col, row) {
 			this.radius = radius;
-			this.centerX = centerX ;
-			this.centerY = centerY ;
+			this.centerX = centerX;
+			this.centerY = centerY;
 			this.col = col;
 			this.row = row;
 			this.points = calculateHexagonPoints(radius, centerX, centerY);
@@ -129,11 +120,9 @@
 	}
 </script>
 
-
 <div id="hexGrid" />
 
 <style>
-
 	#hexGrid {
 		width: 100%;
 		height: 100%;
@@ -145,9 +134,7 @@
 		width: 100%;
 		height: 100%;
 		background: pink;
-
 	}
-
 
 	:global(.hex:hover) {
 		fill: hsla(205, 100%, 50%, 0.75);
