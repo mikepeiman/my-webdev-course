f45288656084f32497ccbfa0aec386b350c27f28
Mike Peiman <mikepeiman@gmail.com>
2023-12-20 14:04:59 -0500

    get grid to fit perfectly, much quicker than before

--- src/routes/exercises/2023-12-20/+page.svelte
+++ src/routes/exercises/2023-12-20/+page.svelte
@@ -8,7 +8,13 @@
 	$: console.log(`🚀 ~ file: Canvas.svelte:11 ~ global_width:`, global_width);
 	$: console.log(`🚀 ~ file: Canvas.svelte:12 ~ global_height:`, global_height);
 
-//get DPI
+$: settings = {
+	radius: 60,
+	numHexesInRow: 5,
+	numHexesInCol: 5,
+	hexWidth: 103.92304845413264,
+	hexHeight: 120,
+}
 
 
 	onMount(() => {
@@ -20,18 +26,19 @@
 		console.log('destroyed');
 	});
 
-	$: hex = calculateHexagonPoints(60, 106/2, 62, true);
+	$: hex = calculateHexagonPoints(settings.radius, settings.hexWidth, settings.hexHeight, true);
 	$: hexes = []
 	$: console.log(`🚀 ~ file: Canvas.svelte:19 ~ hex:`, hex);
 
 	const generateHexGridCoordinates = () => {
-		for(let i = 0; i < 10; i++){
-			for(let j = 0; j < 10; j++){
-				let x = i * 60 * 2 + 60;
-				let y = j * 60 * 2 + 60;
+		for(let i = 0; i < settings.numHexesInRow; i++){
+			for(let j = 0; j < settings.numHexesInCol; j++){
+				let x = i * settings.hexWidth + settings.hexWidth;
+				let y = j * settings.hexHeight * 3/4 + settings.hexHeight * 3/4;
 				if(j % 2 === 1){
 					x += 106/2;
 				}
+	
 				const hex = calculateHexagonPoints(60, x, y);
 				console.log(`🚀 ~ file: Canvas.svelte:29 ~ generateHexGridCoordinates ~ hex:`, hex)				
 				hexes = [...hexes, hex];
