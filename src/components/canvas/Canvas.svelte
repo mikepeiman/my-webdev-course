66083ceded42695977cc42a9c42cd443e4c1cb42
Mike Peiman <mikepeiman@gmail.com>
2023-12-20 13:51:31 -0500

    improved Canvas rendering component to sharpen drawings

--- src/components/canvas/Canvas.svelte
+++ src/components/canvas/Canvas.svelte
@@ -1,57 +1,67 @@
 <script lang="ts">
+	import { onMount, setContext, onDestroy } from 'svelte';
+	import type { DrawFn } from './types';
+	let clearFrames = true;
+	let frameId: number;
+	let canvasElement: HTMLCanvasElement;
+	let fnsToDraw = [] as DrawFn[];
+	$: dpi = 4;
 
-    import { onMount, setContext, onDestroy } from "svelte";
-    import type { DrawFn } from "./types";
-    let clearFrames = true
-    let frameId: number
-  let canvasElement: HTMLCanvasElement;
-  let fnsToDraw = [] as DrawFn[];
+	setContext('canvas', {
+		addDrawFn: (fn: DrawFn) => {
+			fnsToDraw.push(fn);
+		},
+		removeDrawFn: (fn: DrawFn) => {
+			let index = fnsToDraw.indexOf(fn);
+			if (index > -1) {
+				fnsToDraw.splice(index, 1);
+			}
+		}
+	});
 
-  setContext("canvas", {
-    addDrawFn: (fn: DrawFn) => {
-      fnsToDraw.push(fn);
-    },
-    removeDrawFn: (fn: DrawFn) => {
-        let index = fnsToDraw.indexOf(fn);
-        if (index > -1){
-        fnsToDraw.splice(index, 1);
-        }
-    },
-  });
-  
-    onMount(() => {
-      // get canvas context
-      let ctx = canvasElement.getContext("2d");
-      frameId = requestAnimationFrame(() => draw(ctx));
-    });
-  
-    onDestroy(() => {
-      if (frameId){
-          cancelAnimationFrame(frameId)
-      }
-    })
-  
-    function draw(ctx: CanvasRenderingContext2D, clearFrames) {
-      if (clearFrames) {
-          ctx.clearRect(0,0,canvasElement.width, canvasElement.width)
-      }
-      fnsToDraw.forEach((fn) => fn(ctx));
-      frameId = requestAnimationFrame(() => draw(ctx))
-    }
+	onMount(() => {
+		dpi = window.devicePixelRatio;
+		// get canvas context
+		let ctx = canvasElement.getContext('2d');
+		let style_height = +getComputedStyle(canvasElement).getPropertyValue('height').slice(0, -2);
+		//get CSS width
+		let style_width = +getComputedStyle(canvasElement).getPropertyValue('width').slice(0, -2);
+		canvasElement.setAttribute('height', style_height * dpi);
+		canvasElement.setAttribute('width', style_width * dpi);
+		frameId = requestAnimationFrame(() => draw(ctx));
+	});
 
-    function registerMouseEvents(e) {
-      console.log(`🚀 ~ file: Canvas.svelte:43 ~ registerMouseEvents ~ e:`, e)
-      
-    }
-  </script>
+	onDestroy(() => {
+		if (frameId) {
+			cancelAnimationFrame(frameId);
+		}
+	});
 
-<canvas id="canvas1" on:mousemove={registerMouseEvents} on:mouseleave={registerMouseEvents} bind:this={canvasElement} />
+	function draw(ctx: CanvasRenderingContext2D, clearFrames) {
+		if (clearFrames) {
+			ctx.clearRect(0, 0, canvasElement.width, canvasElement.width);
+		}
+		fnsToDraw.forEach((fn) => fn(ctx));
+		frameId = requestAnimationFrame(() => draw(ctx));
+	}
+
+	function registerMouseEvents(e) {
+		// console.log(`🚀 ~ file: Canvas.svelte:43 ~ registerMouseEvents ~ e:`, e);
+	}
+</script>
+
+<canvas
+	id="canvas1"
+	on:mousemove={registerMouseEvents}
+	on:mouseleave={registerMouseEvents}
+	bind:this={canvasElement}
+/>
 <slot />
 
 <style>
-  #canvas1 {
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-  }
+	#canvas1 {
+		width: 100%;
+		height: 100%;
+		background-color: #fff;
+	}
 </style>
