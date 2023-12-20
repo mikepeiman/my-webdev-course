54e59cc56aca635e6ef8de9f80efddf06664d913
Mike Peiman <mikepeiman@gmail.com>
2023-12-20 13:55:00 -0500

    created hex grid, quicker and cleaner than before. but still not fitting properly

--- src/routes/exercises/2023-12-20/+page.svelte
+++ src/routes/exercises/2023-12-20/+page.svelte
@@ -8,8 +8,12 @@
 	$: console.log(`🚀 ~ file: Canvas.svelte:11 ~ global_width:`, global_width);
 	$: console.log(`🚀 ~ file: Canvas.svelte:12 ~ global_height:`, global_height);
 
+//get DPI
+
+
 	onMount(() => {
 		console.log('mounted');
+		generateHexGridCoordinates()
 	});
 
 	onDestroy(() => {
@@ -17,7 +21,23 @@
 	});
 
 	$: hex = calculateHexagonPoints(60, 106/2, 62, true);
+	$: hexes = []
 	$: console.log(`🚀 ~ file: Canvas.svelte:19 ~ hex:`, hex);
+
+	const generateHexGridCoordinates = () => {
+		for(let i = 0; i < 10; i++){
+			for(let j = 0; j < 10; j++){
+				let x = i * 60 * 2 + 60;
+				let y = j * 60 * 2 + 60;
+				if(j % 2 === 1){
+					x += 106/2;
+				}
+				const hex = calculateHexagonPoints(60, x, y);
+				console.log(`🚀 ~ file: Canvas.svelte:29 ~ generateHexGridCoordinates ~ hex:`, hex)				
+				hexes = [...hexes, hex];
+			}
+		}
+	}
 
 
 </script>
@@ -26,6 +46,8 @@
 
 <main>
 	<Canvas>
-		<Hex {hex} />
+		{#each hexes as hex}
+			<Hex {hex} />
+		{/each}
 	</Canvas>
 </main>
